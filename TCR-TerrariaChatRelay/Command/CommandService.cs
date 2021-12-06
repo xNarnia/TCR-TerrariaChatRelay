23ec0b8ef07626912464a41987462c59f95f12bd
Panini <princessnarnia1@gmail.com>
2021-12-06 10:41:30 -0500

    Fix command service breaking for small inputs

--- TCR-TerrariaChatRelay/Command/CommandService.cs
+++ TCR-TerrariaChatRelay/Command/CommandService.cs
@@ -23,6 +23,9 @@ namespace TerrariaChatRelay
 		/// <returns>True if a command key is found. False if not.</returns>
 		public bool IsCommand(string input, string commandPrefix)
 		{
+			if (input.Length <= commandPrefix.Length)
+				return false;
+
 			if(input.Length > commandPrefix.Length)
 			{
 				if (!input.StartsWith(commandPrefix))
